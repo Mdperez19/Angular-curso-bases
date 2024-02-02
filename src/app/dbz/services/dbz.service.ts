326259a05b8154d031c63bc726917e6f58d4071c
Mdperez19 <mdperezpriego@gmail.com>
2024-02-01 23:42:08 -0600

    Refactor MainPageComponent to use DbzService

---
+++ src/app/dbz/services/dbz.service.ts
@@ -0,0 +1,29 @@
+import { Injectable } from "@angular/core";
+import { Character } from "../interfaces/character.interface";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DbzService {
+  public characters: Character[] = [{
+    name: 'Krillin',
+    power: 1000
+  },{
+    name: 'Goku',
+    power: 9500
+  },{
+    name: 'Vegeta',
+    power: 7500
+  }];
+
+  onNewCharacter(character: Character) {
+    console.log('Main Page')
+    console.log(character);
+    this.characters.push(character);
+  }
+
+  onDeleteCharacter(index: number) {
+    console.log({index});
+    this.characters.splice(index, 1);
+  }
+}
