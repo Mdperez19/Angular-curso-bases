3fd8d9ed2541366f784bf1272692819e3334fa46
Mdperez19 <mdperezpriego@gmail.com>
2024-02-02 00:43:34 -0600

    Add UUID package and generate unique IDs for characters

--- src/app/dbz/services/dbz.service.ts
+++ src/app/dbz/services/dbz.service.ts
@@ -1,17 +1,22 @@
 import { Injectable } from "@angular/core";
 import { Character } from "../interfaces/character.interface";
 
+import {v4 as uuid} from 'uuid';
+
 @Injectable({
   providedIn: 'root'
 })
 export class DbzService {
   public characters: Character[] = [{
+    id: uuid(),
     name: 'Krillin',
     power: 1000
   },{
+    id: uuid(),
     name: 'Goku',
     power: 9500
   },{
+    id: uuid(),
     name: 'Vegeta',
     power: 7500
   }];
@@ -19,7 +24,12 @@ export class DbzService {
   onNewCharacter(character: Character) {
     console.log('Main Page')
     console.log(character);
-    this.characters.push(character);
+
+    const newCharacter = {
+      ...character, // Spread operator to copy the object
+      id: uuid()
+    };
+    this.characters.push(newCharacter);
   }
 
   onDeleteCharacter(index: number) {
